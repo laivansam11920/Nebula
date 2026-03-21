3b8b3dc7097f96a5f8c189cb2ac3545e57d9c657
laivansam <samvasang1192011@gmail.com>
2026-03-21 22:33:12 +0700

    test: link cdn js delivr(4)

--- frontend/javascript/group_password/main/forgot_password3.js
+++ frontend/javascript/group_password/main/forgot_password3.js
@@ -1,4 +1,4 @@
-import { showToast } from '../../popup/popup.js';
+import { showToast } from 'https://cdn.jsdelivr.net/gh//gemini-dot/learnpythonserver-sm@main/frontend/javascript/popup/popup.js';
 
 (function () {
   const socket = io(
