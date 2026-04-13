d9f1eb8d6d9c831f27ee47fe1b70dc07424c188b
admin_sam <samvasang1192011@gmail.com>
2026-04-13 12:39:49 +0700

    update: cấp domain(update)

--- backend/routes/group_chuc_nang/upload/chuc_nang/show_html_domain.py
+++ backend/routes/group_chuc_nang/upload/chuc_nang/show_html_domain.py
@@ -13,7 +13,6 @@ def hien_thi_trang_ca_nhan(username, duong_dan_file):
     link_github_goc = f"https://gemini-dot.github.io/html_upload/users/{username}/{duong_dan_file}"
 
     try:
-
         res, code = lock_file_services()
 
         if code != 200:
@@ -21,15 +20,21 @@ def hien_thi_trang_ca_nhan(username, duong_dan_file):
                 return send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"), 401
             return send_from_directory(thu_muc_chinh("frontend/view/error"), "500.html"), 500
 
-        lay_file = requests.get(link_github_goc, timeout=20)
+        with requests.get(link_github_goc, stream=True, timeout=20) as lay_file:
+            
+            if lay_file.status_code == 404:
+                return "Lỗi rách việc: Không tìm thấy trang của ông kẹ này!", 404
+            
+            kieu_file = lay_file.headers.get('content-type', 'text/html')
+            
+            def truyen_du_lieu():
+                for chunk in lay_file.iter_content(chunk_size=8192):
+                    if chunk:
+                        yield chunk
 
-        if lay_file.status_code == 404:
-            return "Lỗi rách việc: Không tìm thấy trang của ông kẹ này!", 404
-        
-        return Response(
-            lay_file.content, 
-            mimetype=lay_file.headers.get('content-type', 'text/html')
-        ), 200
+            return Response(truyen_du_lieu(), mimetype=kieu_file), 200
+            
+    except requests.exceptions.Timeout:
+         return "Lỗi rách việc: Github bên Mỹ xa xôi, tui chờ 20 giây hết nổi rồi!", 504
     except Exception as e:
         return f"Lỗi hệ thống người trung gian: {e}", 500
-    
